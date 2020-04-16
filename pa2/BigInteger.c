485bea9e152fd00be689a642f09f0c9736e78a7e
Jordan Mai <jmai12@unix2.lt.ucsc.edu>
2020-04-15 23:37:50 -0700

    save

--- pa2/BigInteger.c
+++ pa2/BigInteger.c
@@ -19,9 +19,16 @@ typedef struct BigIntegerObj {
 BigInteger newBigInteger() {
   BigInteger b = malloc(sizeof(BigIntegerObj));
   b->sign = 0;
+  b->carry = 0;
   b->L = newList();
 
   return b;
+}
+
+void freeBigInteger(BigInteger *pN) {
+  if (pN != NULL && *pN != NULL) {
+    free(pN);
+  }
 }
 
 int sign(BigInteger N) {
@@ -35,6 +42,40 @@ int sign(BigInteger N) {
     }
   }
   exit(1);
+}
+
+int compare(BigInteger A, BigInteger B) {
+  if (sign(A) == 1 && sign(B) == -1)
+    return 1;
+  if (sign(A) == -1 && sign(B) == 1)
+    return -1;
+
+  if (length(A->L) > length(B->L))
+    return 1;
+  if (length(A->L) < length(B->L))
+    return -1;
+
+  if (front(A->L) > front(B->L))
+    return 1;
+  if (front(A->L) < front(B->L))
+    return -1;
+
+  if (front(A->L) == front(B->L))
+    return 0;
+}
+
+int equals(BigInteger A, BigInteger B) {
+  if (compare(A, B) == 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+void makeZero(BigInteger N) {
+  l_clear(N->L);
+  N->carry = 0;
+  N->sign = 0;
 }
 
 void negate(BigInteger N) {
@@ -78,33 +119,45 @@ BigInteger stringToBigInteger(char *s) {
     }
     prepend(b->L, atol(temp2));
   }
-  printList(stdout, b->L);
-  printf("\n");
-
+  moveFront(b->L);
+  while (get(b->L) == 0) {
+    deleteFront(b->L);
+    moveFront(b->L);
+  }
   return b;
 }
 
 BigInteger copy(BigInteger N) {
   BigInteger b = newBigInteger();
   b->L = copyList(N->L);
-  printList(stdout, b->L);
+  b->sign = N->sign;
 
   return b;
 }
 
 void add(BigInteger S, BigInteger A, BigInteger B) {
+  makeZero(S);
+  BigInteger AA = copy(A);
+  BigInteger BB = copy(B);
 
-  if ((A->sign == 1 && B->sign == 1) || (A->sign == -1 && B->sign == -1)) {
+  if ((AA->sign == 1 && B->sign == -1)) {
+    negate(BB);
+    subtract(S, AA, BB);
+  } else if (AA->sign == -1 && BB->sign == 1) {
+    negate(AA);
+    subtract(S, BB, AA);
+
+  } else {
     int carry = 0, temp = 0, a = 0, b = 0;
-
-    moveBack(A->L);
-    moveBack(B->L);
-    temp = length(A->L) >= length(B->L) ? length(A->L) : length(B->L);
+    moveBack(AA->L);
+    moveBack(BB->L);
+    temp = length(AA->L) >= length(BB->L) ? length(AA->L) : length(BB->L);
 
     for (int i = 0; i < temp; i++) {
       int ab = 0;
-      a = l_index(A->L) != -1 ? get(A->L) : EMPTY;
-      b = l_index(B->L) != -1 ? get(B->L) : EMPTY;
+      a = l_index(AA->L) != -1 ? get(AA->L) : EMPTY;
+      b = l_index(BB->L) != -1 ? get(BB->L) : EMPTY;
+
       if (a == EMPTY) {
         ab += 0;
       } else {
@@ -129,15 +182,15 @@ void add(BigInteger S, BigInteger A, BigInteger B) {
       if (carry == 1) {
         S->carry = 1;
       }
-      if (get(A->L) == EMPTY) {
+      if (get(AA->L) == EMPTY) {
         a = EMPTY;
       } else {
-        movePrev(A->L);
+        movePrev(AA->L);
       }
-      if (get(B->L) == EMPTY) {
+      if (get(BB->L) == EMPTY) {
         b = EMPTY;
       } else {
-        movePrev(B->L);
+        movePrev(BB->L);
       }
       carry = 0;
     }
@@ -145,7 +198,11 @@ void add(BigInteger S, BigInteger A, BigInteger B) {
       S->carry = 0;
       prepend(S->L, 1);
     }
-    printList(stdout, S->L);
+    if (AA->sign == -1 && BB->sign == -1) {
+      S->sign = -1;
+    } else {
+      S->sign = 1;
+    }
   }
 }
 
@@ -156,4 +213,171 @@ BigInteger sum(BigInteger A, BigInteger B) {
     return b;
   }
   return NULL;
+}
+
+void subtract(BigInteger D, BigInteger A, BigInteger B) {
+  BigInteger AA, BB;
+  if (compare(A, B) == -1) {
+    AA = copy(B);
+    BB = copy(A);
+    D->sign = -1;
+  } else {
+    AA = copy(A);
+    BB = copy(B);
+  }
+
+  negate(BB);
+  if ((sign(AA) == 1 && sign(BB) == 1) || (sign(AA) == -1 && sign(BB) == -1)) {
+    add(D, AA, BB);
+  } else {
+    int carry = 0, temp = 0, a = 0, b = 0;
+
+    moveBack(AA->L);
+    moveBack(BB->L);
+    temp = length(AA->L) >= length(BB->L) ? length(AA->L) : length(BB->L);
+
+    for (int i = 0; i < temp; i++) {
+      int ab = 0;
+      a = l_index(AA->L) != -1 ? get(AA->L) : EMPTY;
+      b = l_index(BB->L) != -1 ? get(BB->L) : EMPTY;
+
+      if (a == EMPTY) {
+        ab += 0;
+      } else {
+        if (sign(AA) == 1) {
+          ab += a;
+        } else {
+          ab -= a;
+        }
+      }
+      if (b == EMPTY) {
+        ab += 0;
+      } else {
+        if (sign(BB) == 1) {
+          ab += b;
+        } else {
+          ab -= b;
+        }
+      }
+      if (i > 0 && D->carry == -1) {
+        ab -= 1;
+        D->carry = 0;
+      }
+
+      if (ab < 0) {
+        ab += BASE;
+        carry = -1;
+      }
+
+      prepend(D->L, ab);
+      if (carry == -1) {
+        D->carry = -1;
+      }
+      if (get(AA->L) == EMPTY) {
+        a = EMPTY;
+      } else {
+        movePrev(AA->L);
+      }
+      if (get(BB->L) == EMPTY) {
+        b = EMPTY;
+      } else {
+        movePrev(BB->L);
+      }
+      carry = 0;
+    }
+    if (D->carry == -1) {
+      D->sign = -1;
+    }
+  }
+}
+
+BigInteger diff(BigInteger A, BigInteger B) {
+  if (A && B) {
+    BigInteger b = newBigInteger();
+    subtract(b, A, B);
+    return b;
+  }
+  return NULL;
+}
+
+void multiply(BigInteger P, BigInteger A, BigInteger B) {
+  BigInteger AA = copy(A);
+  BigInteger BB = copy(B);
+  makeZero(P);
+
+  P->sign = sign(AA) * sign(BB);
+  int a = 0, b = 0, ab = 0, carry = 0, temp = 0;
+  moveBack(AA->L);
+  moveBack(BB->L);
+
+  BigInteger big, small;
+  temp = length(AA->L) <= length(BB->L) ? length(AA->L) : length(BB->L);
+
+  if (length(AA->L) >= length(BB->L)) {
+    big = AA;
+    small = BB;
+  } else {
+    big = BB;
+    small = AA;
+  }
+
+  moveBack(big->L);
+  moveBack(small->L);
+  BigInteger b_temp = newBigInteger();
+
+  for (int i = 0; i < temp; i++) {
+    b_temp = newBigInteger();
+    ab = 0, carry = 0;
+    a = l_index(big->L) != EMPTY ? get(big->L) : EMPTY;
+    b = l_index(small->L) != EMPTY ? get(small->L) : EMPTY;
+
+    while (get(big->L) != EMPTY) {
+      a = get(big->L);
+      if (b == EMPTY) {
+        b = 1;
+      } else {
+        ab = a * b;
+      }
+
+      if (b_temp->carry > 0) {
+        ab += b_temp->carry;
+        b_temp->carry = 0;
+      }
+
+      if (ab >= BASE) {
+        carry = ab / BASE;
+        ab -= carry * BASE;
+        b_temp->carry = carry;
+      }
+
+      prepend(b_temp->L, ab);
+      movePrev(big->L);
+    }
+    if (b_temp->carry > 0) {
+      prepend(b_temp->L, b_temp->carry);
+      b_temp->carry = 0;
+    }
+    for (int j = 0; j < i; j++) {
+      append(b_temp->L, 0);
+    }
+    movePrev(small->L);
+    moveBack(big->L);
+    add(P, P, b_temp);
+  }
+}
+
+BigInteger prod(BigInteger A, BigInteger B) {
+  if (A && B) {
+    BigInteger b = newBigInteger();
+    multiply(b, A, B);
+    return b;
+  }
+}
+
+void printBigInteger(FILE *out, BigInteger N) {
+  if (sign(N) == -1) {
+    printf("-");
+  }
+  printList(out, N->L);
+  printf("\n");
 }
