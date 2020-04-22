5f76791e1a6ee5516ce0194d81d1fa5541c7e03e
Jordan Mai <jmai12@unix2.lt.ucsc.edu>
2020-04-21 21:04:11 -0700

    first

---
+++ pa3/Graph.c
@@ -0,0 +1,167 @@
+#include "Graph.h"
+#include "List.h"
+#define EMPTY -1
+
+typedef struct GraphObj {
+  List *list;
+  int *color;
+  int *distance;
+  int *parent;
+  int order;
+  int size;
+  int source;
+} GraphObj;
+
+Graph newGraph(int n) {
+  Graph g = malloc(sizeof(GraphObj));
+  // linked list for neighbors
+  g->list = calloc(n + 1, sizeof(List));
+  // b,w,g
+  g->color = calloc(n + 1, sizeof(GraphObj));
+  g->distance = calloc(n + 1, sizeof(GraphObj));
+  // contain predecessor
+  g->parent = calloc(n + 1, sizeof(GraphObj));
+  g->source = 0;
+  g->size = n;
+  g->order = 0;
+  return g;
+}
+void freeGraph(Graph *pG) {
+  if (pG != NIL && *pG != NIL) {
+  }
+}
+
+nt getOrder(Graph G) {
+  if (G == NULL) {
+    printf("ERROR on getSize");
+    exit(1);
+  }
+  return G->order;
+}
+
+int getSize(Graph G) {
+  if (G == NULL) {
+    printf("ERROR on getSize");
+    exit(1);
+  }
+  return G->size;
+}
+
+int getSource(Graph G) {
+  if (G == NULL) {
+    printf("ERROR on GetSource()");
+    exit(1);
+  }
+  if (G->source != 0) {
+    return G->source;
+  }
+  return NIL;
+}
+
+int getParent(Graph G, int u) {
+  if (G == NULL) {
+    printf("ERROR on getParent()");
+    exit(1);
+  }
+  return G->parent[u];
+}
+
+int getDist(Graph G, int u) {
+  if (G == NULL) {
+    printf("ERROR on getDist");
+    exit(1);
+  }
+  return G->distance[u];
+}
+
+void getPath(List L, Graph G, int u) {
+  if (getSource(G) == NIL) {
+    printf("ERROR on getPath() .no source found");
+    exit(1);
+  }
+  if (getSource(G) == u) {
+    prepend(L, u);
+  } else if (G->parent[u] == NIL) {
+    printf("The distance from %d to %d is infinity", getSource(G), u);
+  } else {
+    getPath(L, G, getParent(G, u));
+    prepend(L, u);
+  }
+}
+
+void addEdge(Graph G, int u, int v) {
+  if (G == NIL) {
+    printf("ERROR on addEDGE()");
+    exit(1);
+  }
+  if (length(G->list[u]) == -1) {
+    G->list[u] = newList();
+    prepend(G->list[u], v);
+  } else {
+    prepend(G->list[u], v);
+  }
+  if (length(G->list[v]) == -1) {
+    G->list[v] = newList();
+    prepend(G->list[v], u);
+  } else {
+    prepend(G->list[v], u);
+  }
+  G->order++;
+}
+
+void addArc(Graph G, int u, int v) {
+  if (G == NIL) {
+    printf("ERROR on addArc()");
+    exit(1);
+  }
+  if (length(G->list[u]) == -1) {
+    G->list[u] = newList();
+    prepend(G->list[u], v);
+  } else {
+    prepend(G->list[u], v);
+  }
+  G->order++;
+}
+
+void BFS(Graph G, int s) {
+  for (int i = 0; i <= getSize(G); i++) {
+    if (length(G->list[i]) > 0) {
+      G->color[i] = 'w';
+      G->distance[i] = INF;
+      G->parent[i] = NIL;
+    }
+  }
+  G->parent[s] = NIL;
+  G->color[s] = 'g';
+  G->distance[s] = 0;
+  G->source = s;
+
+  List l = newList();
+  prepend(l, s);
+  while (length(l) > 0) {
+    int num = front(l);
+    deleteFront(l);
+    moveFront(G->list[num]);
+    while (get(G->list[num]) != EMPTY) {
+      if (G->color[get(G->list[num])] == 'w') {
+        G->color[get(G->list[num])] = 'g';
+        G->distance[get(G->list[num])] = G->distance[num] + 1;
+        G->parent[get(G->list[num])] = num;
+        append(l, get(G->list[num]));
+      }
+      moveNext(G->list[num]);
+    }
+    G->color[num] = 'b';
+  }
+  freeList(&l);
+}
+
+void printGraph(FILE *out, Graph G) {
+  for (int i = 1; i <= G->size; i++) {
+    if (length(G->list[i]) > 0) {
+      printf("%d: ", i);
+      printList(out, G->list[i]);
+      printf("\n");
+    }
+  }
+}
---
+++ pa3/List.h
@@ -0,0 +1,51 @@
+/****************************
+ *  * Jordan Mai, jmai12
+ *  * 2020 Spring CSE101,PA1
+ *  * List.h
+ *  * Header File for List
+ *******************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef _LIST_H_
+#define _LIST_H_
+
+typedef struct ListObj *List;
+
+List newList(void);      // Creates and returns a new empty List.
+void freeList(List *pL); // Frees all heap memory associated with *pL, and sets
+
+long length(List L); // Returns the number of elements in L.
+long index(List L); // Returns index of cursor element if defined, -1 otherwise.
+long front(List L); // Returns front element of L. Pre: length()>0
+long back(List L);  // Returns back element of L. Pre: length()>0
+long get(List L);   // Returns cursor element of L. Pre: length()>0, index()>=0
+long l_equals(List A, List B);
+
+void clear(List L);
+
+void moveFront(List L);
+void moveBack(List L);
+void movePrev(List L);
+
+void moveNext(List L);
+void prepend(List L, long data);
+
+void append(List L, long data);
+
+void insertBefore(List L, long data);
+
+void insertAfter(List L, long data);
+
+void deleteFront(List L);
+void deleteBack(List L); // Delete the back element. Pre: length()>0
+void delete (List L);
+void printList(FILE *out, List L);
+
+List copyList(List L);
+
+void set(List L, long x);
+
+// List concatList(List A, List B);
+
+#endif
