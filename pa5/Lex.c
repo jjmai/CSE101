c911caa9593b81c6236af4c08418a2871491d6aa
Jordan Mai <jmai12@unix6.lt.ucsc.edu>
2020-05-11 17:27:34 -0700

    10/10

--- pa5/Lex.c
+++ pa5/Lex.c
@@ -3,7 +3,7 @@
  * 2020 Spring CSE101 PA5
  * Lex.c
  * Program to sort alphabetically
- **********************************/
+ ***********************************/
 
 #include "Dictionary.h"
 #include <stdio.h>
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
   char lines;
-  int count = 0;
+  int count=0;
   int array_index = 0;
   FILE *infile;
   FILE *outfile;
@@ -25,24 +25,28 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
   outfile = fopen(argv[2], "w");
-  // count number of lines
+
   while ((lines = fgetc(infile)) != EOF) {
     if (lines == '\n') {
       count++;
     }
   }
-  char array[count][255];
   fclose(infile);
   infile = fopen(argv[1], "r");
-  // store each word in array
-  while ((fgets(array[array_index], sizeof(array[array_index]), infile))) {
-    array_index++;
-  }
 
   Dictionary d = newDictionary(0);
-  for (int i = 0; i < array_index; i++) {
-    insert(d, array[i], i);
+  char **ptr;
+  ptr = calloc(count, sizeof(char *));
+  for (int i = 0; i < count; i++) {
+    ptr[i] = calloc(255, sizeof(char));
   }
+  while (fgets(ptr[array_index],255,infile)){
+    array_index++;
+  }
+  for (int i = 0; i < array_index; i++) {
+    insert(d, ptr[i], i);
+  }
+
   beginForward(d);
   for (int i = 0; i < size(d); i++) {
     fprintf(outfile, "%s", currentKey(d));
