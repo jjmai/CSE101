903a7e03f724bc2e643b66c85195bec4b042d80e
Jordan Mai <jmai12@unix3.lt.ucsc.edu>
2020-05-19 20:49:21 -0700

    aded t.nil

--- pa6/Dictionary.c
+++ pa6/Dictionary.c
@@ -1,9 +1,3 @@
-/*********************************
- * Jordan Mai, jmai12
- * 2020 Spring CSE101 PA6
- * Dictionary ADT
- *********************************/
-
 #include "Dictionary.h"
 #include <assert.h>
 #include <stdbool.h>
@@ -29,12 +23,15 @@ typedef NodeObj *Node;
 
 void pre(FILE *out,Dictionary D, Node z);
 void post(FILE *out,Dictionary D, Node z);
+void rbTransplant(Dictionary D,Node x, Node y);
+void rbDeleteFixUp(Dictionary D, Node x);
 
 typedef struct DictionaryObj {
     Node root;
     int unique;
     int size;
     Node cursor; // for move functions
+    Node nil;
 } DictionaryObj;
 
 Node newNode(char *k, int *v) {
@@ -42,7 +39,7 @@ Node newNode(char *k, int *v) {
     assert(n);
     n->key = k;
     n->value = v;
-    n->color = 0;
+    n->color = 'b';
     n->left = NULL;
     n->right = NULL;
     n->parent=NULL;
@@ -67,9 +64,10 @@ void deleteAll(Node R) {
 Dictionary newDictionary(int unique) {
     Dictionary d = malloc(sizeof(struct DictionaryObj));
     assert(d);
-    d->root = NULL;
     d->size = 0;
-    d->cursor = NULL;
+    d->nil=newNode(KEY_UNDEF,VAL_UNDEF);
+    d->root = d->nil;
+    d->cursor = d->nil;
     if (unique == 0) {
         d->unique = 0;
     } else {
@@ -99,14 +97,12 @@ int getUnique(Dictionary D) {
         printf("ERROR get UNIQU");
         exit(1);
     }
-    // 1 == duplicates
     if (D->unique == 1) {
         return 1;
     } else {
         return 0;
     }
 }
-
 Node findKey(Node n, KEY_TYPE k) {
     if (n == NULL || KEY_CMP(k, n->key) == 0) {
         return n;
@@ -123,7 +119,7 @@ VAL_TYPE lookup(Dictionary D, KEY_TYPE k) {
         printf("ERROR on lookup");
         exit(1);
     }
-    Node n;
+    Node n=NULL;
     n = findKey(D->root, k);
     if (n != NULL) {
         return n->value;
@@ -140,15 +136,15 @@ void insert(Dictionary D, KEY_TYPE k, VAL_TYPE v) {
     D->size++;
 
 }
+
 void delete (Dictionary D, KEY_TYPE k) {
     if (D == NULL) {
         printf("ERROR on delete");
         exit(1);
     }
-    if(lookup(D,k)!=VAL_UNDEF) {
+    rbDelete(D,k);
+    D->size--;
 
-        D->size--;
-    }
 
 }
 
@@ -158,7 +154,6 @@ void makeEmpty(Dictionary D) {
         exit(1);
     }
     deleteAll(D->root);
-
     D->root = NULL;
     D->size = 0;
     D->cursor = NULL;
@@ -179,7 +174,6 @@ VAL_TYPE beginForward(Dictionary D) {
     }
     return VAL_UNDEF;
 }
-
 VAL_TYPE beginReverse(Dictionary D) {
     if (D == NULL) {
         printf("error on beginReverse");
@@ -195,6 +189,7 @@ VAL_TYPE beginReverse(Dictionary D) {
     }
     return VAL_UNDEF;
 }
+
 KEY_TYPE currentKey(Dictionary D) {
     if (D == NULL) {
         printf("ERROR ON currentKEY");
@@ -205,6 +200,7 @@ KEY_TYPE currentKey(Dictionary D) {
     }
     return KEY_UNDEF;
 }
+
 VAL_TYPE currentVal(Dictionary D) {
     if (D == NULL) {
         printf("ERROR ON currentKEY");
@@ -215,6 +211,7 @@ VAL_TYPE currentVal(Dictionary D) {
     }
     return VAL_UNDEF;
 }
+
 VAL_TYPE next(Dictionary D) {
     if (D == NULL) {
         printf("ERROR on next");
@@ -244,6 +241,7 @@ VAL_TYPE next(Dictionary D) {
     D->cursor = temp;
     return temp->value;
 }
+
 VAL_TYPE prev(Dictionary D) {
     if (D == NULL) {
         fprintf(stderr, "ERROR on prev");
@@ -304,183 +302,35 @@ void post(FILE *out,Dictionary D, Node z) {
     printf("%s\n",z->key);
 }
 
-void leftRotate(Dictionary D, KEY_TYPE x) {
-    if( D==NULL) {
-        printf("ERROR on leftrotate");
-        exit(1);
-    }
-    Node A = D->root;
-    Node y;
-    while(KEY_CMP(x,A->key)!=0) {
-        if(KEY_CMP(x,A->key)<0) {
-            A=A->left;
-        } else {
-            A=A->right;
-        }
-    }
-    y=A->right;
-    A->right=y->left;
-    if(y->left != NULL) {
-        y->left->parent=A;
-    }
-    y->parent=A->parent;
-    if(A->parent==NULL) {
-        D->root=y;
-    } else if(A==A->parent->left) {
-        A->parent->left=y;
-    } else {
-        A->parent->right=y;
-    }
-    y->left=A;
-    A->parent=y;
-
-}
-void rightRotate(Dictionary D, KEY_TYPE x) {
-    if(D==NULL) {
-        printf("ERROR on right rotate");
-        exit(1);
-    }
-    Node A = D->root;
-    Node y= NULL;
-    while (KEY_CMP(x, A->key) != 0) {
-        if (KEY_CMP(x, A->key) < 0) {
-            A = A->left;
-        } else  {
-            A = A->right;
-        }
-    }
-    y = A->left;
-    A->left = y->right;
-    if (y->right != NULL) {
-        y->right->parent = A;
-    }
-    y->parent = A->parent;
-    if (A->parent == NULL) {
-        D->root = y;
-    } else if (A == A->parent->right) {
-        A->parent->right = y;
-    } else {
-        A->parent->left = y;
-    }
-    y->right = A;
-    A->parent = y;
-}
-
 void rbInsert(Dictionary D, KEY_TYPE z) {
     if(D==NULL) {
         printf("ERROR on rbinsert");
         exit(1);
     }
     if(getUnique(D)==1) {
-        if(lookup(D,z)==VAL_UNDEF) {
-            Node y= NULL;
-            Node A= D->root;
-            Node new = newNode(z,NULL);
-            while(A!=NULL) {
-                y=A;
-                if(KEY_CMP(z,A->key)<0) {
-                    A=A->left;
-                } else if(KEY_CMP(z,A->key)>0) {
-                    A=A->right;
-                }
+        Node y= D->nil;
+        Node A= D->root;
+        Node new = newNode(z,NULL);
+        while(A != D->nil) {
+            y=A;
+            if(KEY_CMP(z,A->key)<0) {
+                A=A->left;
+            } else if(KEY_CMP(z,A->key)>=0) {
+                A=A->right;
             }
-            new->parent=y;
-            if(y==NULL) {
-                new->color='b';
-                D->root=new;
-            } else if(KEY_CMP(new->key,y->key)<0) {
-                y->left=new;
-            } else {
-                y->right =new;
-            }
-            new->left=NULL;
-            new->right =NULL;
-            new->color='r';
-            rbInsertFixUp(D,new->key);
         }
-    }
-}
-
-void rbInsertFixUp(Dictionary D, KEY_TYPE z ) {
-    if(D==NULL) {
-        printf("ERROR on rbinsertfixup");
-        exit(1);
-    }
-    Node A = D->root;
-    Node y= NULL;
-    while(KEY_CMP(z,A->key)!=0) {
-        if(KEY_CMP(z,A->key)<0) {
-            A=A->left;
-        } else if (KEY_CMP(z,A->key)>0) {
-            A=A->right;
-        }
-    }
-    while(A->parent !=NULL &&  A->parent->color =='r') {
-        if(A->parent==A->parent->parent->left) {
-            y=A->parent->parent->right;
-            if(y!=NULL && y->color=='r') {
-                A->parent->color='b';
-                y->color='b';
-                A->parent->parent->color='r';
-                A=A->parent->parent;
-            } else {
-                if(A==A->parent->right) {
-                    A=A->parent;
-                    leftRotate(D,A->key);
-                }
-                A->parent->color='b';
-                A->parent->parent->color='r';
-                rightRotate(D,A->parent->parent->key);
-            }
+        new->parent=y;
+        if(y==D->nil) {
+            new->color='b';
+            D->root=new;
+        } else if(KEY_CMP(new->key,y->key)<0) {
+            y->left=new;
         } else {
-            y=A->parent->parent->left;
-            if(y !=NULL && y->color=='r') {
-                A->parent->color='b';
-                y->color='b';
-                A->parent->parent->color='r';
-                A=A->parent->parent;
-            } else {
-                if(A==A->parent->left) {
-                    A=A->parent;
-                    rightRotate(D,A->key);
-                }
-                A->parent->color='b';
-                A->parent->parent->color='r';
-                leftRotate(D,A->parent->parent->key);
+            y->right =new;
+        }
+        new->left=D->nil;
+        new->right =D->nil;
+        new->color='r';
+        rbInsertFixUp(D,new->key);
             }
-        }
-    }
-    D->root->color='b';
-}
-
-void rbTransplant(Dictionary D,KEY_TYPE x, KEY_TYPE y) {
-    if( D==NULL) {
-        printf("ERROR on rbTransplant");
-        exit(1);
-    }
-    Node A= D->root;
-    Node B= D->root;
-    while(KEY_CMP(x,A->key)!=0) {
-        if(KEY_CMP(x,A->key)<0) {
-            A=A->left;
-        } else {
-            A=A->right;
-        }
-    }
-    while(KEY_CMP(x,B->key)!=0) {
-        if(KEY_CMP(x,B->key)<0) {
-            B=B->left;
-        } else {
-            B=B->right;
-        }
-    }
-
-}
-
-void rbDelete(Dictionary D, KEY_TYPE z) {
-    if(D==NULL) {
-        printf("error on rb Delete");
-        exit(1);
-    }
-
-
+            }
